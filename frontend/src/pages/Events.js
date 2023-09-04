a4cc23cb4c7a425e73fb77d742f8f81a27a0af8d
Voodoo30fps <voodoo30fps@gmail.com>
2023-09-04 21:44:34 +0300

    error handling with custom errors, extracting error data, throwing responses

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -6,16 +6,26 @@ function EventsPage() {
   const data = useLoaderData();
   const events = data.events;
 
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+
   return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/events');
+  const response = await fetch('http://localhost:8080/eventss');
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: 'Could not fetch events.' };
+    // // ^ just to display a message or something similar
+
+    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+      status: 500,
+    });
+    // ^ Will use the closest error page from the router
   } else {
     return response;
   }
